f6eb92528629c33b15a4baec539399be2d03deb5
shuntaro <shun.sea.f@gmail.com>
2020-07-21 12:02:08 +0900

    posts index

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,29 @@
 <?php
-  include('./header.php');
+  require_once( "../config/dbconnect.php" );
+  dbconnection();
+
   session_start();
+
+  include('./header.php');
+  
 ?>
   <body>
     <h1 class="main-title">SHUN BBS</h1>
     <?php
       if (isset($_SESSION['id'])) {//ログインしているとき
-        echo '<a class="btn" href="logout.php">ログアウト</a>';
+        echo '<div><a class="btn" href="logout.php">ログアウト</a></div>';
+        echo '<div><a class="btn" href="post_form.php">投稿する</a></div>';
+
+        $sql = "SELECT * FROM posts";
+        $posts = $pdo->query($sql);
+
+        foreach ($posts as $post) {
+          $date = new DateTime($post['created_at']);
+          echo '<a href="post_show.php?id=' . $post['id'] .  '">' . $date->format('Y/n/d G:i ');
+          echo ' 投稿者：' . $post['user_name'];
+          echo '<br> title：' . $post['title'] . '</a>';
+          echo '<br>';
+        }
       } else {//ログインしていない時
         echo '<a class="btn" href="pre_registration_form.php">サインアップ</a>
         <br>
