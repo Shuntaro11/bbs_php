5e7b681ebb4bd8c93581df95fbda6239b5c4d5c1
shuntaro <shun.sea.f@gmail.com>
2020-07-21 13:06:27 +0900

    jsによるバリデーション

--- public_html/post_form.php
+++ public_html/post_form.php
@@ -19,16 +19,21 @@ include('./header.php');
       <form action="post.php" method="POST">
       <div>
           <label for="title">タイトル</label>
-          <p><input type="text" id="title" name="title" placeholder="タイトルを30文字以内で入力してください"></p>
+          <p><input type="text" id="title" name="title" placeholder="タイトルを30文字以内で入力してください" onblur="judgeTitle()"></p>
+          <p class="error-massage" id="title_error"></p>
       </div>
       <div>
           <label for="content">本文</label>
-          <p><textarea cols="50" rows="10" id="content" name="content" placeholder="本文を入力してください"></textarea></p>
+          <p><textarea cols="50" rows="10" id="content" name="content" placeholder="本文を入力してください" onblur="judgeContent()"></textarea></p>
+          <p class="error-massage" id="content_error"></p>
       </div>
           <input type="hidden" name="token" value="<?=$token?>">
           <input class="btn submit-btn" type="submit" class="btn" value="投稿">
       </form>
       <a class="btn" href="index.php">ホーム</a>
     </div>
+
+    <script type="text/javascript" src="./js/post_form_validation.js"></script>
+
   </body>
 </html>
