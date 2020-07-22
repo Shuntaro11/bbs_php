988aa8ba92c8020a5f29c58d924a41d9551f1226
shuntaro <shun.sea.f@gmail.com>
2020-07-22 16:16:56 +0900

    aggregate posts and users

--- public_html/index.php
+++ public_html/index.php
@@ -12,19 +12,40 @@
       $page = 1;
   }
 
+  // 今月の投稿数を取得
+  $sql = "SELECT COUNT(id) FROM posts GROUP BY DATE_FORMAT(created_at, '%Y%m')";
+  $howManyPosts = $pdo->query($sql)->fetchColumn();
+
+  // 今月の登録者数を取得
+  $sql = "SELECT COUNT(id) FROM users GROUP BY DATE_FORMAT(created_at, '%Y%m')";
+  $howManyUsers = $pdo->query($sql)->fetchColumn();
+
+  // このページの投稿を取得
+  $offset = POSTS_PER_PAGE * ($page - 1);
+  $sql = "SELECT posts.id AS post_id, posts.created_at AS created_at, user_name, title FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY post_id DESC limit " . $offset . "," . POSTS_PER_PAGE;
+  $posts = $pdo->query($sql);
+
+  // ページ数を取得
+  $total = $pdo->query("select count(*) from posts")->fetchColumn();
+  $totalPages = ceil($total / POSTS_PER_PAGE);
+
+  // ○件目〜○件目を取得
+  $from = $offset + 1;
+  $to = ($offset + POSTS_PER_PAGE) < $total ? ($offset + POSTS_PER_PAGE) : $total;
+
   include('./header.php');
   
 ?>
   <body>
     <h1 class="main-title">SHUN BBS</h1>
+    <div class="aggregate-table">
+      <p>会員登録者数 <?PHP echo $howManyUsers ?> 人/月</p>
+      <p>累計投稿数 <?PHP echo $howManyPosts ?> 件/月</p>
+    </div>
     <?php
       if (isset($_SESSION['id'])) {//ログインしているとき
         echo '<div><a class="btn" href="logout.php">ログアウト</a></div>';
         echo '<div><a class="btn" href="post_form.php">投稿する</a></div>';
-
-        $offset = POSTS_PER_PAGE * ($page - 1);
-        $sql = "SELECT posts.id AS post_id, posts.created_at AS created_at, user_name, title FROM posts INNER JOIN users ON posts.user_id = users.id ORDER BY post_id DESC limit " . $offset . "," . POSTS_PER_PAGE;
-        $posts = $pdo->query($sql);
 
         echo '<div class="top-text">投稿一覧</div>';
 
@@ -34,12 +55,6 @@
           echo '<div class="each-post"><a href="post_show.php?id=' . $post['post_id'] .  '">' . $date->format('Y/n/d G:i ') . '<br> 投稿者：' . $post['user_name'] . '<br> title：' . $post['title'] . '</a></div>';
 
         }
-        
-        $total = $pdo->query("select count(*) from posts")->fetchColumn();
-        $totalPages = ceil($total / POSTS_PER_PAGE);
-
-        $from = $offset + 1;
-        $to = ($offset + POSTS_PER_PAGE) < $total ? ($offset + POSTS_PER_PAGE) : $total;
 
         for($i = 1; $i <= $totalPages; $i++){
           echo '<a class="page-link" href="?page=' . $i .'">' . $i . '</a>';
